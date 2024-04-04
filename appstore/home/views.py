2e95090383896e99c95ad3417042b1dd9acc3f31
Abdul Nayeem Khalid <abdulnayeem2013@gmail.com>
2024-04-04 21:05:42 +0530

    Jenkins Plz Work

--- appstore/home/views.py
+++ appstore/home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello World !!!</h1> Jenking is Working!")
+    return HttpResponse("<h1>Hello World !!!</h1> Jenking is Working??")
 # Create your views here.
