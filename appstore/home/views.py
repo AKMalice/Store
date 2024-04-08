f841f4d25ceae49fdf63ba13568bb34e67c68a17
Abdul Nayeem Khalid <abdulnayeem2013@gmail.com>
2024-04-08 10:51:52 +0530

    Appstore V1.0

--- appstore/home/views.py
+++ appstore/home/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello World !!!</h1> Jenking is Working??")
+    return render(request, 'home/homepage.html')
+    # return HttpResponse("<h1>Hello World !!!</h1> Jenking is Working??")
 # Create your views here.
